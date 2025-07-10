14960ae42d40b1e8a1eeb2f6ee7ef2a866bcbdc2
PierreTDX <pierre.tondeux@gmail.com>
2025-07-10 18:19:25 +0200

    Coorection bug sur CardProject a dans a

--- components/CardProject/CardProject.tsx
+++ components/CardProject/CardProject.tsx
@@ -11,38 +11,41 @@ type Props = {
 };
 
 export default function CardProject({ project }: Props) {
-    return (
-        <a href={project.site} target="_blank" rel="noopener noreferrer">
 
-            <div className="cardProject">
-                <div className="cardImageWrapper">
-                    <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={400}
-                        height={200}
-                        className="cardImage"
-                    />
-                </div>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <ul className="tags">
-                    {project.tags.map((tag, index) => (
-                        <li key={index} className="tag">
-                            {tag}
-                        </li>
-                    ))}
-                </ul>
-                <div className="cardLinks">
-                    <a href={project.github} target="_blank" rel="noopener noreferrer">
-                        <IoLogoGithub size={28} />
-                    </a>
-                    <a href={project.site} target="_blank" rel="noopener noreferrer" className="site">
-                        <FaExternalLinkAlt size={24} />
-                    </a>
-                </div>
+    const handleCardClick = () => {
+        window.open(project.site, "_blank");
+    };
+
+    return (
+
+        <article className="cardProject" onClick={handleCardClick} role="link" tabIndex={0}>
+            <div className="cardImageWrapper">
+                <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={400}
+                    height={200}
+                    className="cardImage"
+                />
             </div>
-        </a>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <ul className="tags">
+                {project.tags.map((tag, index) => (
+                    <li key={index} className="tag">
+                        {tag}
+                    </li>
+                ))}
+            </ul>
+            <div className="cardLinks">
+                <a href={project.github} target="_blank" rel="noopener noreferrer">
+                    <IoLogoGithub size={28} />
+                </a>
+                <a href={project.site} target="_blank" rel="noopener noreferrer" className="site">
+                    <FaExternalLinkAlt size={24} />
+                </a>
+            </div>
+        </article>
 
     );
 }
